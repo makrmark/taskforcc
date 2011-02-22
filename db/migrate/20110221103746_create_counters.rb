3f0425c6e333e03c0fd6e0f37624ba6cb824ff92
Mark Cowlishaw <mark.cowlishaw@gmail.com>
2011-02-22 17:49:00 +0100

    And more

--- db/migrate/20110221103746_create_counters.rb
+++ db/migrate/20110221103746_create_counters.rb
@@ -15,7 +15,7 @@ class CreateCounters < ActiveRecord::Migration
     end
 
     execute "INSERT INTO counters (user_id, collaboration_id, topic_id, " +
-        "cnt_new, cnt_assigned, cnt_accepted, cnt_resolved, cnt_rejected, cnt_closed, cTotal) " +
+        "cnt_new, cnt_assigned, cnt_accepted, cnt_resolved, cnt_rejected, cnt_closed, cnt_total) " +
       "SELECT assigned_to, collaboration_id, topic_id, " +
       "SUM ( CASE WHEN status='New'       THEN 1 ELSE 0 END ) as cnt_new, " +
       "SUM ( CASE WHEN status='Assigned'  THEN 1 ELSE 0 END ) as cnt_assigned, " +
